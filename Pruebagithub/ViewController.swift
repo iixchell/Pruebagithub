adcab8f5f79d1d424a440b727f88fb151a9ce05c
iixchell <ixchel.ra.01@hotmail.com>
2023-03-07 13:43:11 -0600

    Initial Commit

---
+++ Pruebagithub/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Pruebagithub
+//
+//  Created by CDAM15 on 07/03/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
